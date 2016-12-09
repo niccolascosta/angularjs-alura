5c2bcb6d9f50d0b12b4e23367015df1d1e3d41aa
niccolas <niccolas.costa@gmail.com>
2016-12-09 21:29:44 -0200

    comite inicial

---
+++ public/js/directives/minhas-diretivas.js
@@ -0,0 +1,28 @@
+angular.module('minhasDiretivas', [])
+    .directive('meuPainel', function() {
+
+        var ddo = {};
+
+        ddo.restrict = "AE";
+        ddo.transclude = true;
+
+        ddo.scope = {
+            titulo: '@'
+        };
+
+        ddo.templateUrl = 'js/directives/meu-painel.html';
+
+        return ddo;
+    })
+    .directive('minhaFoto', function(){
+
+        var ddo = {
+            restrict: "AE",
+            scope: {
+                titulo:'@',
+                url:'@'
+            },
+            templateUrl:'js/directives/minha-foto.html'
+        };
+        return ddo;
+    });
