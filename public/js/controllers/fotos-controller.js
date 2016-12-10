49b0e33f031c0388bde2fe6d7db04a966a46ee38
niccolas <niccolas.costa@gmail.com>
2016-12-10 00:19:44 -0200

    melhoria/ fazendo a as aulas 10 e 11 do curso do alura.

--- public/js/controllers/fotos-controller.js
+++ public/js/controllers/fotos-controller.js
@@ -1,31 +1,24 @@
-angular.module('alurapic').controller('FotosController', function ($scope, $http) {
+angular.module('alurapic').controller('FotosController', function ($scope, recursoFoto) {
 
     $scope.fotos = [];
     $scope.filtro = '';
     $scope.mensagem = '';
 
-    $http.get('/v1/fotos')
-        .success(function (fotos) {
-            console.log(fotos);
-            $scope.fotos = fotos;
-        })
-        .error(function (erro) {
-            console.log(erro);
-        });
+    recursoFoto.query(function(fotos) {
+        $scope.fotos = fotos;
+    }, function(erro) {
+        console.log(erro);
+    });
 
     $scope.remover = function(foto) {
-
-        $http.delete('/v1/fotos/' + foto._id)
-            .success(function() {
-                var indiceDaFoto = $scope.fotos.indexOf(foto);
-                $scope.fotos.splice(indiceDaFoto, 1);
-                $scope.mensagem = 'Foto ' + foto.titulo + ' removida com sucesso!';
-
-            })
-            .error(function(erro) {
-                console.log(erro);
-                $scope.mensagem = 'Não foi possível apagar a foto ' + foto.titulo;
-            });
+        recursoFoto.delete({fotoId: foto._id}, function() {
+            var indiceDaFoto = $scope.fotos.indexOf(foto);
+            $scope.fotos.splice(indiceDaFoto, 1);
+            $scope.mensagem = 'Foto ' + foto.titulo + ' removida com sucesso!';
+        }, function(erro) {
+            console.log(erro);
+            $scope.mensagem = 'Não foi possível apagar a foto ' + foto.titulo;
+        });
     };
 
     /*var promise = $http.get('/v1/fotos');
